ecaef465901175a29bbd6714c1bb542b45f50322
Luana Barth <luanamaiarabbarth@gmail.com>
2024-11-26 08:01:09 -0300

    excluir invoicy

--- cypress/integration/automatiza invoicy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="Cypress" />
-
-describe('teste no invoicy homolog', function() {
-    it('verifica o título da aplicação', function() {
-        cy.visit ('https://homolog.invoicy.com.br/login.aspx') 
-    })
-  })
-  
